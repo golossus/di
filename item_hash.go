c0ee2f41cfb5bf1da7f9469c42cbe45d441d2c76
Santiago Garcia <sangarbe@gmail.com>
2021-09-14 00:24:54 +0200

    adds license header on source code files

--- item_hash.go
+++ item_hash.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Santiago Garcia <sangarbe@gmail.com>.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package di
 
 import "fmt"
